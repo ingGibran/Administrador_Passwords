719a8ce16a35f06490a011ecd75ba6156a5fffee
gibran <zaga1709@hotmail.com>
2025-06-09 13:49:20 -0600

    Agregamos la ruta a static

--- practica_login/settings.py
+++ practica_login/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -123,5 +124,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 ALLOWED_HOSTS = ['administrador-contraseanas.onrender.com']
+
